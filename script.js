996042dc7c4de51ae1715ac5d3c1040897476c0f
songhyunfoodstuffsltd <songhyunfoodstuffsltd@gmail.com>
2021-09-10 14:10:58 +0300

    songhyunfoodstaff

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-// let menu = document.querySelector('#menu-bar');
-// let navbar = document.querySelector('.navbar');
-
-// menu.onclick = () =>{
-
-//   menu.classList.toggle('fa-times');
-//   navbar.classList.toggle('active');
-
-// }
-
-// window.onscroll = () =>{
-
-//   menu.classList.remove('fa-times');
-//   navbar.classList.remove('active');
-
-//   if(window.scrollY > 60){
-//     document.querySelector('#scroll-top').classList.add('active');
-//   }else{
-//     document.querySelector('#scroll-top').classList.remove('active');
-//   }
-
-// }
-
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 
@@ -121,13 +98,3 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-
-function loader(){
-  document.querySelector('.loader-container').classList.add('fade-out');
-}
-
-function fadeOut(){
-  setInterval(loader, 3000);
-}
-
-window.onload = fadeOut;
